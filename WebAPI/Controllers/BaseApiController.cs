8879099bcc44183c15ab0ba16a2da7741a2c1a37
Felix Wetell <felix.wetell@gmail.com>
2022-06-02 22:00:46 +0200

    Adds base controller to implement basic functionality for all controllers

---
+++ WebAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    /// <summary>
+    /// Base API controller.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        /// <summary>
+        /// Base API controller constructor.
+        /// </summary>
+        public BaseApiController() : base()
+        {
+
+        }
+    }
+}
