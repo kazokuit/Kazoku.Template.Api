1380ac32332e1a0ebc0bcbaae460b57b0f3a9599
Felix Wetell <felix.wetell@gmail.com>
2022-06-02 17:33:37 +0200

    Adds basic program logic with swagger options

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,25 +1,85 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+using WebApi.SwaggerOptions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Variables
+string apiName = "Template Web API";
 
+// Versioning
+builder.Services.AddApiVersioning(config =>
+{
+    config.DefaultApiVersion = ApiVersion.Parse("2022-04-01");
+    config.AssumeDefaultVersionWhenUnspecified = true;
+    config.ReportApiVersions = true;
+    config.ApiVersionReader = new QueryStringApiVersionReader();
+});
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "G";
+    options.DefaultApiVersionParameterDescription = "API Version in YYYY-MM-DD format";
+});
+
+// Swagger options
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+var xmlFilePath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+builder.Services.AddSwaggerGen(options =>
+{
+    options.IncludeXmlComments(xmlFilePath);
+});
+
+
+// Logging
+builder.Logging.ClearProviders();
+builder.Services.AddLogging(options =>
+{
+    options.AddSimpleConsole(c =>
+    {
+        c.TimestampFormat = "[yyyy-MM-dd HH:mm:ss] ";
+        //c.UseUtcTimestamp = true;
+    });
+});
+
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
+// Building the application
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.Logger.LogWarning($"--- ALERT: {apiName} is in development mode. ---");
 }
 
-app.UseHttpsRedirection();
+// Using Swagger UI
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    foreach (var desc in apiVersionDescriptionProvider.ApiVersionDescriptions)
+    {
+        var title = $"{apiName} version {desc.ApiVersion}";
 
+        options.DocumentTitle = title;
+        options.SwaggerEndpoint($"{desc.GroupName}/swagger.json", title);
+        options.DefaultModelsExpandDepth(-1);
+        options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+    }
+});
+
+// Final setup
 app.UseAuthorization();
-
 app.MapControllers();
+app.Logger.LogInformation($"{apiName} is now running.");
 
+// Running application
 app.Run();
